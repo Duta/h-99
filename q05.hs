a4cff44bbe80d03344c314af3b626ea93fa738ab
Bertie Wheen <wheen.b@gmail.com>
2014-11-18 02:49:10 +0000

    Update q05.hs

--- q05.hs
+++ q05.hs
@@ -1,5 +1,10 @@
 module Q05 where
 
+import Data.List (foldl')
+
 myReverse :: [a] -> [a]
 myReverse []    = []
 myReverse (h:t) = myReverse t ++ [h]
+
+myReverse' :: [a] -> [a]
+myReverse' = foldl' (flip (:)) []
